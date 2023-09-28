1a967db6b33cc074acde1cfe721cb9d00986560d
Isaac <54126919+Aizakkuno@users.noreply.github.com>
2023-09-28 17:54:35 -0400

    slight accuracy change

--- src/ControlConcept.java
+++ src/ControlConcept.java
@@ -23,7 +23,7 @@ public class ControlConcept {
         double ly = flattenAxesValue(axes.ly);
         double lx = flattenAxesValue(axes.lx);
 
-        double power = flattenAxesValue(Math.sqrt((lx * lx) + (ly * ly)) * (int) ly);
+        double power = Math.sqrt((lx * lx) + (ly * ly)) * (int) ly;
 
         double leftMultiplier = (lx + 1.0) / 2.0;
         double rightMultiplier = 1.0 - leftMultiplier;
@@ -55,62 +55,7 @@ public class ControlConcept {
             XInputComponents aComponents = aController.getComponents();
             XInputAxes aAxes = aComponents.getAxes();
 
-<<<<<<< Updated upstream
-            int leftWheelPower;
-            int rightWheelPower;
-
-            double ly = aAxes.ly;
-            double lx = aAxes.lx;
-
-            if (aAxes.ly > 1.0) {
-                ly = 1.0;
-            } else if (aAxes.ly < -1.0) {
-                ly = -1.0;
-            } else if (aAxes.ly < 0.1 && aAxes.ly > 0) {
-                ly = 0.0;
-            } else if (aAxes.ly > -0.1 && aAxes.ly < 0) {
-                ly = 0.0;
-            }
-
-            if (aAxes.lx > 1.0) {
-                lx = 1.0;
-            } else if (aAxes.lx < -1.0) {
-                lx = -1.0;
-            } else if (aAxes.lx < 0.1 && aAxes.lx > 0) {
-                lx = 0.0;
-            } else if (aAxes.lx > -0.1 && aAxes.lx < 0) {
-                lx = 0.0;
-            }
-
-            double power = Math.sqrt((lx * lx) + (ly * ly));
-            if (power > 1.0) {
-                // may not be perfect because the middle edges (inbetween straight down, straight 
-                // right, etc) are ~0.1 over 1
-                power = 1.0;
-            }
-
-            double moreSteer = Math.abs(lx);
-            if (moreSteer == 0) {
-                moreSteer = 0.5;
-            }
-
-            double lessSteer = 1.0 - moreSteer;
-
-            int leftPower;
-            int rightPower;
-
-            if (lx < 0) {
-                leftPower = (int) (power * (100 * lessSteer));
-                rightPower = (int) (power * (100 * moreSteer));
-            } else {
-                leftPower = (int) (power * (100 * moreSteer));
-                rightPower = (int) (power * (100 * lessSteer));
-            }
-
-            System.out.print("Left: " + leftPower + ", Right: " + rightPower + "            \r");
-=======
             System.out.println(getWheelPowers(aAxes));
->>>>>>> Stashed changes
         }
     }
 }
