11da50cb133ccf241f6bc2e1309e1e24c84c8f7a
Isaac <54126919+Aizakkuno@users.noreply.github.com>
2023-09-28 17:49:56 -0400

    should work better?

--- src/ControlConcept.java
+++ src/ControlConcept.java
@@ -5,6 +5,40 @@ import com.github.strikerx3.jxinput.*;
 import java.lang.Math;
 
 public class ControlConcept {
+    public static double flattenAxesValue(double value) {
+        if (value > 1.0) {
+            value = 1.0;
+        } else if (value < -1.0) {
+            value = -1.0
+        } else if (value > 0.0 && value < 0.1) {
+            value = 0.0
+        } else if (value < -0.0 && value > -0.1) {
+            value = 0.0
+        }
+
+        return value
+    }
+
+    public static int[] getWheelPowers(XInputAxes axes) {
+        double ly = flattenAxesValue(axes.ly);
+        double lx = flattenAxesValue(axes.lx);
+
+        double power = flattenAxesValue(Math.sqrt((lx * lx) + (ly * ly)) * (int) ly);
+
+        double leftMultiplier = (lx + 1.0) / 2.0;
+        double rightMultiplier = 1.0 - leftMultiplier;
+
+        double leftPower = leftMultiplier * power;
+        double rightPower = rightMultiplier * power;
+
+        int leftPowerPercent = (int) (100 * leftPower);
+        int rightPowerPercent = (int) (100 * rightPower);
+
+        int[] powers = {leftPowerPercent, rightPowerPercent};
+
+        return powers
+    }
+
     public static void main(String[] args) throws Exception {
         if (!XInputDevice.isAvailable()) {
             System.out.println("XInput is not available!");
@@ -19,9 +53,9 @@ public class ControlConcept {
             }
 
             XInputComponents aComponents = aController.getComponents();
-
             XInputAxes aAxes = aComponents.getAxes();
 
+<<<<<<< Updated upstream
             int leftWheelPower;
             int rightWheelPower;
 
@@ -74,6 +108,9 @@ public class ControlConcept {
             }
 
             System.out.print("Left: " + leftPower + ", Right: " + rightPower + "            \r");
+=======
+            System.out.println(getWheelPowers(aAxes));
+>>>>>>> Stashed changes
         }
     }
 }
