e95552bb74851cb6e6ae3c8ca62bc803d5b38248
Isaac <54126919+Aizakkuno@users.noreply.github.com>
2023-10-03 08:27:09 -0400

    working light hp system

--- src/LightsConcept.java
+++ src/LightsConcept.java
@@ -2,9 +2,13 @@ public class LightsConcept {
     int[][] COLORS = {{0, 0, 0}, {255, 0, 0}, {255, 255, 0}, {255, 85, 0}, {0, 255, 0}};
 
     public static void setTailLights(Finch finch, int hp) {
+        if (hp < 0) {
+            return;
+        }
+
         int lightsEnabled = (int) (hp / 25);
-        for (int i = 0; i < lightsEnabled; i++) {
-            finch.setTail(i, 0, 255, 0);
+        for (int i = 1; i < lightsEnabled + 1; i++) {
+            finch.setTail(i, 0, 100, 0);
         }
 
         if (lightsEnabled == 4) {
@@ -19,9 +23,9 @@ public class LightsConcept {
             lastLightLevel = (int) (lastLightHP / 6);
         }
 
-        int r;
-        int g;
-        int b;
+        int r = 0;
+        int g = 0;
+        int b = 0;
         switch (lastLightLevel) {
             case 0:
                 r = 0;
@@ -30,39 +34,49 @@ public class LightsConcept {
                 break;
         
             case 1:
-                r = 255;
+                r = 100;
                 g = 0;
                 b = 0;
                 break;
 
             case 2:
-                r = 255;
-                g = 85;
+                r = 100;
+                g = 40;
                 b = 0;
                 break;
 
             case 3:
-                r = 255;
-                g = 255;
+                r = 100;
+                g = 100;
                 b = 0;
                 break;
 
             case 4:
                 r = 0;
-                g = 255;
+                g = 100;
                 b = 0;
                 break;
 
             default:
                 break;
         }
+
+        finch.setTail(lightsEnabled + 1, r, g, b);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Finch finch = new Finch("A");
 
+        int hp = 100;
+
         while (true) {
-            setTailLights(finch, 50);
+            setTailLights(finch, hp);
+
+            System.out.println(hp);
+
+            hp--;
+
+            Thread.sleep(100);
         }
     }
 }
